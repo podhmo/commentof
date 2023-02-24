cc9a32b176b1e539f52f285545986552b2032ba4
podhmo <ababjam61+github@gmail.com>
2023-02-25 08:23:47 +0900

    chore: update example

--- testdata/regression/issue16.go
+++ testdata/regression/issue16.go
@@ -3,4 +3,4 @@ package regression
 // Deletes a pet by ID
 //
 // deletes a single pet based on the ID supplied
-func DeletePet(input DeletePetInput) struct{}/* pet deleted TODO: */ { return struct{}{} }
+func DeletePet(input DeletePetInput) struct{}/* pet deleted */ { return struct{}{} }
